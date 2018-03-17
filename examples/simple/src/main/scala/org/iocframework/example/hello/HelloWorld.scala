c9d0bf5e30541a2110cfe282eff8f169e85be10e
david-sledge <sledged@gmail.com>
2018-03-17 04:30:49 -0600

    Add a bit more to the example

--- examples/simple/src/main/scala/org/iocframework/example/hello/HelloWorld.scala
+++ examples/simple/src/main/scala/org/iocframework/example/hello/HelloWorld.scala
@@ -1,8 +1,12 @@
 package org.iocframework.example.hello
 
+import java.util.Calendar
+
 class HelloWorld(message: String)
 {
-   def getMessage(){
-      println(s"Your Message : $message")
-   }
+  private val madeOnTimeStamp = Calendar.getInstance().getTime
+
+  def getMessage(){
+    println(s"Your Message : $message.  The time is now $madeOnTimeStamp")
+  }
 }
--- examples/simple/src/main/scala/org/iocframework/example/hello/MainApp.scala
+++ examples/simple/src/main/scala/org/iocframework/example/hello/MainApp.scala
@@ -4,7 +4,37 @@ import org.iocframework.staffFactoryFromResource
 import scala.ioc.Factory
 
 object MainApp extends App {
+  def printEq(obj1: Any, obj2: Any) = {
+    println(if (obj1 == obj2) "Same object" else "Not the same object")
+  }
+
+  // instantiate the factory
   val (factory, _) = staffFactoryFromResource("FactoryStaff.sfs")
-  val obj = factory.putToWork("helloWorld").asInstanceOf[HelloWorld]
-  obj.getMessage()
+  // put the manager to work and get the fruits of her labor
+  val obj1 = factory.putToWork("helloWorld1").asInstanceOf[HelloWorld]
+  obj1.getMessage()
+
+  Thread sleep 2000
+  // since this manager is lazy, she won't put any effort to produce a new deliverable.
+  // She'll just give us what she provided the first time
+  val obj1Again = factory.putToWork("helloWorld1").asInstanceOf[HelloWorld]
+  obj1Again.getMessage()
+
+  printEq(obj1, obj1Again)
+
+  // This guy, however, is like an excitable puppy who's eager to please
+  val obj2 = factory.putToWork("helloWorld2").asInstanceOf[HelloWorld]
+  obj2.getMessage()
+  Thread sleep 2000
+  // Task him again, and he'll work to give something new
+  val differentObj2 = factory.putToWork("helloWorld2").asInstanceOf[HelloWorld]
+  differentObj2.getMessage()
+
+  printEq(obj2, differentObj2)
+
+  // Even though the first manager is lazy, you can still make her work hard
+  val differentObj1 = factory.crackTheWhip("helloWorld1").asInstanceOf[HelloWorld]
+  differentObj1.getMessage()
+
+  printEq(obj1, differentObj1)
 }
